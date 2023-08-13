99fe3554be72d465f2c9a7c0b1f6eb0a7d77df3c
fatemesalmani <fatemesalmaniii3876@gmail.com>
2023-08-14 00:00:51 +0330

    my new tailwind task

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      maxWidth: {
+        '1/2': '50%',
+      }
+    },
+  },
+  plugins: [],
+}
+
